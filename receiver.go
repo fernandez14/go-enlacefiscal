110a187f4a00f6c85bcdc704fd5cedcef76e3206
fernandez14 <fernandez14@outlook.com>
2016-04-29 12:42:24 -0500

    [add] Receiver address

--- receiver.go
+++ receiver.go
@@ -1,6 +1,19 @@
 package efiscal
 
 type Receiver struct {
-	RFC  string `json:"rfc"`
-	Name string `json:"nombre"`
+	RFC     string   `json:"rfc"`
+	Name    string   `json:"nombre"`
+	Address *Address `json:"DomicilioFiscal,omitempty"`
+}
+
+type Address struct {
+	Street       string `json:"calle"`
+	Ext          string `json:"noExterior"`
+	Int          string `json:"noInterior"`
+	Neighborhood string `json:"colonia"`
+	Locality     string `json:"localidad"`
+	Town         string `json:"municipio"`
+	State        string `json:"estado"`
+	Country      string `json:"pais"`
+	Zipcode      string `json:"cp"`
 }
